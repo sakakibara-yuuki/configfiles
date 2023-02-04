0e1b7f912e7e9c7cfd5370a79a1f1dfdba79165b
sakakibara <sakakilabo0000@gmail.com>
2023-02-04 21:52:28 +0900

    add new comment to *.py template

---
+++ =template=.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © %YEAR% %USER% <%MAIL%>
+#
+# Distributed under terms of the %LICENSE% license.
+
+"""
+refactaring
+    重複したコードをメソッドに抽出する
+    インデントが深いコードをメソッドに抽出する
+    引数をまとめてクラスを作る
+    クラスのメソッドに関数を移動する
+    メンバ変数とそれを使うメソッドを分けてクラスにする
+naming
+    argument
+        ロジックを過不足なく表す
+        一語一意
+        状態を表す動詞はsをつける(三単現)
+    method
+        動詞で始める
+        操作対象を主語か目的語に添える
+"""
+%HERE%
